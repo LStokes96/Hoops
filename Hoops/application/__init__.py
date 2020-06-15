4bb19c082ba42e6cb69d84e7a44a9cdc0ed1ae89
Liam <lstokes@qa.com>
2020-06-15 16:37:46 +0000

    Added register page, models, and forms

--- Hoops/application/__init__.py
+++ Hoops/application/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import os
+from os import getenv
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('HOOPS_URI'))
+db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = getenv('KEY')
+
+bcrypt = Bcrypt(app)
 
 from application import routes
--- Hoops/application/routes.py
+++ Hoops/application/routes.py
@@ -1,17 +1,50 @@
-from flask import render_template, url_for
-from application import app 
+from flask import render_template, url_for, redirect
+from application import app, db, bcrypt 
+from application.models import Posts, Users
+from application.forms import PostForm, RegistrationForm
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data)
 
+        user = Users(email=form.email.data, password=hash_pw)
 
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('post'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+                first_name = form.first_name.data,
+                last_name = form.last_name.data,
+                title = form.title.data,
+                content = form.content.data
+
+            )
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
